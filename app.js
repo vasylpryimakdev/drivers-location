da1970ba39db1ebdc5d54249ee845840796d0752
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-05-27 16:48:53 +0300

    Add Express app test with Supertest and update test setup

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+
+// Run this function whenever someone goes to
+// localhost:3050/
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+module.exports = app;
